b569d7db29d33e1ed922bbe0f2ecbb13e1198fdb
Keilwii <39877361+Keilwii@users.noreply.github.com>
2022-02-18 20:40:12 +0200

    Update ft_strcmp.c

--- libft2/ft_strcmp.c
+++ libft2/ft_strcmp.c
@@ -12,12 +12,12 @@
 
 #include "libft.h"
 
-int	ft_strcmp(char *s1, char *s2)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	int	i;
-
-	i = 0;
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
-		i++;
-	return (s1[i] - s2[i]);
+	while (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
+	{
+		s1++;
+		s2++;
+	}
+	return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
 }
